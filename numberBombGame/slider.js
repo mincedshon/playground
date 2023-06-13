4010063cf2e36ea363100e7b5ea8543a507fc845
mincedshon <86765893+mincedshon@users.noreply.github.com>
2023-06-13 22:59:49 +0800

    a

--- numberBombGame/slider.js
+++ numberBombGame/slider.js
@@ -1,50 +1,54 @@
 var generatedNumber = Math.floor(Math.random() * 101);
-var min = 0;
+var min = 1;
 var max = 100;
 var track = document.getElementById("sliderTrack");
+var trackWidth = track.clientWidth;
+generatedNumber = 70;
+
+document.getElementById("rightBoundaryMarker").style.left = +trackWidth + "px";
+
+
 track.style.width = "100%";
 track.style.left = "0%";
 
 function updateMinMax() {
     min = document.getElementById("minThumb").value;
     max = document.getElementById("maxThumb").value;
+    
 }
 
 function checkNumber() {
     var selected = document.getElementById("userThumb").value;
+    var changedSide;
 
     if(selected != generatedNumber) {
         if (selected > generatedNumber) max = +selected-1;
         if (selected < generatedNumber) min = +selected+1;
-        updateTrack();
+
+        updateTrack(changedSide, selected);
+
     } else {
 
     }
 }
 
-function updateTrack() {
+function updateTrack(selected) {
 
-    var tracklength = max - min;
     var trackWidthValue = track.style.width.substring(0, track.style.width.length-1);
     var trackLeftValue = track.style.left.substring(0, track.style.left.length-1);
 
-    if (trackWidthValue > tracklength) {
-        if (trackLeftValue == min) {
-            track.style.width = +trackWidthValue - 1 + "%";
-        } else {
-            track.style.width = +trackWidthValue - 1 + "%";
-            track.style.left = +trackLeftValue + 1 + "%";
-        }
-    }
+    track.style.width = (max - min) / (100 - 1) * (trackWidth - 25)  + 25 + "px";
+    track.style.left = (min - 1) / (100 - 1) * (trackWidth - 25) + "px";
+    
 
-    var minThumb = document.getElementById("minThumb");
-    var maxThumb = document.getElementById("maxThumb");
-
-    document.getElementById("minNumber").innerHTML = min;
-    document.getElementById("maxNumber").innerHTML = max;
+    var leftMarker = document.getElementById("leftBoundaryMarker");
+    var rightMarker = document.getElementById("rightBoundaryMarker");
 
     if (minThumb.value != min) minThumb.value++;
     if (maxThumb.value != max) maxThumb.value--;
+
+    document.getElementById("minNumber").innerHTML = min;
+    document.getElementById("maxNumber").innerHTML = max;
 
     animationId = requestAnimationFrame(updateTrack);
     if (minThumb.value == min && maxThumb.value == max) {
