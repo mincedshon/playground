91b19228d08290c3fc6cbe342ec4a49ce4b45e98
mincedshon <86765893+mincedshon@users.noreply.github.com>
2023-06-14 18:49:00 +0800

    a

--- numberBombGame/slider.js
+++ numberBombGame/slider.js
@@ -1,15 +1,14 @@
 var generatedNumber = Math.floor(Math.random() * 101);
 var min = 1;
 var max = 100;
+var leftMarker = document.getElementById("leftBoundaryMarker");
+var rightMarker = document.getElementById("rightBoundaryMarker");
 var track = document.getElementById("sliderTrack");
-var trackWidth = track.clientWidth;
-generatedNumber = 70;
+var trackWidth = track.offsetWidth;
 
-document.getElementById("rightBoundaryMarker").style.left = +trackWidth + "px";
+document.getElementById("rightBoundaryMarker").style.left = trackWidth;
 
-
-track.style.width = "100%";
-track.style.left = "0%";
+generatedNumber = 40;
 
 function updateMinMax() {
     min = document.getElementById("minThumb").value;
@@ -19,41 +18,59 @@ function updateMinMax() {
 
 function checkNumber() {
     var selected = document.getElementById("userThumb").value;
-    var changedSide;
+    document.getElementById("buttonCheck").disabled = true;
 
     if(selected != generatedNumber) {
         if (selected > generatedNumber) max = +selected-1;
         if (selected < generatedNumber) min = +selected+1;
 
-        updateTrack(changedSide, selected);
-
+        updateTrack();
     } else {
 
     }
 }
 
-function updateTrack(selected) {
+function updateTrack() {
+    var newTrackWidth = ((max - min) / (100 - 1) * (trackWidth - 25)  + 25).toFixed(2);
+    var newTrackLeft = ((min - 1) / (100 - 1) * (trackWidth - 25)).toFixed(2);
+    //var newTrackWidthFromRight = (100-max) / (100 - 1) * (trackWidth - 25) + 25;
+    //track.style.width = (max - min) / (100 - 1) * (trackWidth - 25)  + 25 + "px";
+    //track.style.left = (min - 1) / (100 - 1) * (trackWidth - 25) + "px";
 
-    track.style.width = (max - min) / (100 - 1) * (trackWidth - 25)  + 25 + "px";
-    track.style.left = (min - 1) / (100 - 1) * (trackWidth - 25) + "px";
-    
-    var trackWidthValue = track.style.width.substring(0, track.style.width.length-2);
-    var trackLeftValue = track.style.left.substring(0, track.style.left.length-2);
-
-    var leftMarker = document.getElementById("leftBoundaryMarker");
-    var rightMarker = document.getElementById("rightBoundaryMarker");
-
-    leftMarker.style.left = track.style.left;
-    console.log(+trackWidthValue + +trackLeftValue);
-    rightMarker.style.left = +trackWidthValue + +trackLeftValue + "px";
+    performAnimation(newTrackWidth, newTrackLeft);
 
     document.getElementById("minNumber").innerHTML = min;
     document.getElementById("maxNumber").innerHTML = max;
+}
 
-    /*animationId = requestAnimationFrame(updateTrack);
-    if (minThumb.value == min && maxThumb.value == max) {
-        cancelAnimationFrame(animationId);
-    }*/
+function performAnimation(newTrackWidth, newTrackLeft) {
+    var repeat = false;
+
+    var currentTrackLeft = getComputedStyle(track).left.substring(0, getComputedStyle(track).left.length-2);
+    var currentTrackWidth = getComputedStyle(track).width.substring(0, getComputedStyle(track).width.length-2);
+    if (parseFloat(parseFloat(currentTrackWidth).toFixed(2)) > newTrackWidth) {           
+        track.style.width = +currentTrackWidth - 3;
+        //rightMarker.style.left = +currentTrackLeft + +currentTrackWidth + "px";
+        repeat = true;
+    } else {
+        track.style.width = newTrackWidth;
+    }
+
+    if (parseFloat(parseFloat(currentTrackLeft).toFixed(2)) < newTrackLeft) {
+        console.log("HELO");
+        track.style.left = +currentTrackLeft + 3;
+        //leftMarker.style.left = track.style.left;
+        repeat = true;
+    } else {
+        track.style.left = newTrackLeft;
+    }
+
+    if (repeat) {
+        var raf = requestAnimationFrame(() => performAnimation(newTrackWidth, newTrackLeft));
+    } else {
+        document.getElementById("buttonCheck").disabled = false;
+        cancelAnimationFrame(raf);
+    } 
 }
 
 function updateTextboxThruThumb() {
